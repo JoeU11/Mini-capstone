ac8033a1922c010385a97937e5407bd5832a0939
joe <joe.ulica@gmail.com>
2022-07-28 14:43:34 -0400

    Make products update dynamic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,12 +15,22 @@ class ProductsController < ApplicationController
     render json: product.as_json
   end
 
+  # def update
+  #   product = Product.find_by(id: params[:id])
+  #   product.name = "Full-sized Goal"
+  #   product.price = 300
+  #   product.image_url = "https://kwikgoal.com/wp-content/uploads/2020/01/w2B3006_1-scaled.jpg"
+  #   product.description = "A full sized soccer goal for official matches."
+  #   product.save
+  #   render json: product.as_json
+  # end
+
   def update
     product = Product.find_by(id: params[:id])
-    product.name = "Full-sized Goal"
-    product.price = 300
-    product.image_url = "https://kwikgoal.com/wp-content/uploads/2020/01/w2B3006_1-scaled.jpg"
-    product.description = "A full sized soccer goal for official matches."
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.image_url = params[:image_url] || product.image_url
+    product.description = params[:description] || product.description
     product.save
     render json: product.as_json
   end
